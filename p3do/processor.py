0791d20e0971a13c36a4ac334ab070013a3063e1
ehw <Knuckles500@gmail.com>
2020-12-27 12:55:52 -0500

    Fix mixed case in Panasonic 3DO main executable (launchme)

--- p3do/processor.py
+++ p3do/processor.py
@@ -9,7 +9,14 @@ class P3doIsoProcessor(BaseIsoProcessor):
         return {"disc_type": "cdr"}
 
     def get_exe_filename(self):
-        return "/launchme"
+        try:
+            dir = self.iso_path_reader.get_root_dir()
+            for file in self.iso_path_reader.iso_iterator(dir):
+                file_path = self.iso_path_reader.get_file_path(file)
+                if file_path.lower() == "/launchme":
+                    return file_path
+        except FileNotFoundError:
+            pass
 
     def get_most_recent_file_info(self, exe_date):
         return {}
