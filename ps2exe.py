646a88c2e29718629fdea3dcac825f9abcf80af0
Ian Carpenter <icarpenter@cultnet.net>
2021-02-06 19:51:35 -0500

    Skip track 2 and up for iso files, too

--- ps2exe.py
+++ ps2exe.py
@@ -86,7 +86,7 @@ def process_path(path, disable_contents_checksum, allowed_extensions):
         "ip.bin"
     }
     ignored_filenames = "|".join(ignored_filenames)
-    if re.search(rf"[Tt]rack ?(?:\d?[2-9]|[1-9]\d+)\)?\.bin|({ignored_filenames})|\.({disallowed_extensions})", path):
+    if re.search(rf"[Tt]rack ?(?:\d?[2-9]|[1-9]\d+)\)?\.(?:bin|iso)|({ignored_filenames})|\.({disallowed_extensions})", path):
         return
 
     size = os.path.getsize(path)
