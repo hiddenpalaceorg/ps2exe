607f29b30089f3891d335ddf0c1b5ce873263b2a
Ian Carpenter <icarpenter@cultnet.net>
2023-08-23 16:57:00 -0400

    Xbox: Support GOD containers that provide a sector offset
    Some GOD games offset their sector numbers in the XISO data. The STFS container provides this data

--- xbox/xdvdfs/xdvdfs.py
+++ xbox/xdvdfs/xdvdfs.py
@@ -3,7 +3,7 @@ from xbox.xdvdfs.volume import Volume
 
 
 class XDvdFs:
-    def __init__(self, fp, offset):
+    def __init__(self, fp, offset, sector_offset=0):
         self.fp = fp
-        self.volume = Volume(fp, offset)
+        self.volume = Volume(fp, offset, sector_offset)
         self.root = Directory(fp, self.volume, self.volume.root_directory_sector, None, self.volume.root_directory_size)
