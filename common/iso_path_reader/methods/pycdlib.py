81af878833ff4cb4a77c679c1f9bdcb931b6504e
Sazpaimon <sazpaimon@x-cult.org>
2024-07-14 15:46:57 -0400

    PyCdlib: search through entire iso directory listing for a file as a last resort

--- common/iso_path_reader/methods/pycdlib.py
+++ common/iso_path_reader/methods/pycdlib.py
@@ -117,10 +117,9 @@ class PyCdLibPathReader(ChunkedHashTrait, IsoPathReader):
             except PyCdlibInvalidInput:
                 if path.upper() != path:
                     return self.get_file(path.upper())
-                if b'\xef\xbf\xbd' in path.encode():
-                    for file in self.iso_iterator(self.get_root_dir(), recursive=True):
-                        if path.lower() == self.get_file_path(file).lower():
-                            return file
+                for file in self.iso_iterator(self.get_root_dir(), recursive=True):
+                    if path.lower() == self.get_file_path(file).lower():
+                        return file
                 raise FileNotFoundError(e)
 
     def open_file(self, file):
